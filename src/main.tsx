58d4e963bcf5b716d17a41d3443126a455b4cb15
Eiinu <eiinu@qq.com>
2024-11-14 00:56:20 +0800

    chore: rename pkg name

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // @ts-ignore
-import Editor from '@eiinu/editor'
+import Editor from '@eiinu/editor-core'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
