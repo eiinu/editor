0bc9a0b4ed5f1f639333b2cfc83610d6fc7b2e67
Eiinu <eiinu@qq.com>
2024-12-16 01:17:56 +0800

    chore: add dts for packages/editor-core

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       fileName: 'index'
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'slate', 'slate-react', 'slate-history'],
     },
     emptyOutDir: false
   }
